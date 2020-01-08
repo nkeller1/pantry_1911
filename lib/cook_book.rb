52ca4a0f8cd9c9d58f9efebe7f829f2601aeff26
Nathan Keller <keller.nathan@gmail.com>
2020-01-08 11:54:36 -0700

    Have the right objects for cookbook but my ingredident hash is a bit off

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -11,15 +11,13 @@ class CookBook
   end
 
   def summary
-    book = Hash.new
-    @all_recipies.map do |recipe|
-      if book.has_key?(recipe.name) == true
-        book[:name]
-      end
-        book[:name] = recipe.name
-        book[:details] = recipe.ingredients_required
-        book[:total_calories] = recipe.total_calories
+    @all_recipies.reduce([]) do |acc, recipe|
+      book = Hash.new
+      book[:name] = recipe.name
+      book[:details] = recipe.ingredients_required
+      book[:total_calories] = recipe.total_calories
+      acc << book
+      acc
     end
-    book
   end
 end
