e72900201a86c77905a569b7d6e307c92c429340
Nathan Keller <keller.nathan@gmail.com>
2020-01-08 10:23:28 -0700

    Iteration 2 complete.

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,7 +1,22 @@
 class Recipe
-  attr_reader :name
+  attr_reader :name, :ingredients_required
+
   def initialize(name)
     @name = name
+    @ingredients_required = Hash.new
   end
 
+  def add_ingredient(ingredient, amount)
+    @ingredients_required[ingredient] = amount
+  end
+
+  def amount_required(ingredient)
+    @ingredients_required[ingredient]
+  end
+
+  def total_calories
+    @ingredients_required.sum do |ingredient, amount|
+      ingredient.calories * amount
+    end
+  end
 end
--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -15,7 +15,31 @@ class RecipeTest < Minitest::Test
     assert_instance_of Recipe, @recipe
   end
 
-  def test_it_has_a_name
+  def test_it_has_attributes
     assert_equal "Mac and Cheese", @recipe.name
+    assert_equal ({}), @recipe.ingredients_required
+  end
+
+  def test_it_can_add_ingredients
+    @recipe.add_ingredient(@ingredient1, 2)
+    @recipe.add_ingredient(@ingredient2, 8)
+    expected = {@ingredient1 => 2, @ingredient2 => 8}
+
+    assert_equal expected, @recipe.ingredients_required
+  end
+
+  def test_amount_required
+    @recipe.add_ingredient(@ingredient1, 2)
+    @recipe.add_ingredient(@ingredient2, 8)
+
+    assert_equal 2, @recipe.amount_required(@ingredient1)
+    assert_equal 8, @recipe.amount_required(@ingredient2)
+  end
+
+  def test_total_calories
+    @recipe.add_ingredient(@ingredient1, 2)
+    @recipe.add_ingredient(@ingredient2, 8)
+
+    assert_equal 440, @recipe.total_calories
   end
 end
